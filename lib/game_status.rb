1fdd07c5691342f13d4b8f13f02da16ed46177f2
Mark Drehmann <markdrehmann@gmail.com>
2020-01-04 02:11:00 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,16 +15,22 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.select do |win_combination|
-    win_combination.select do |position|
-      if position[0] == "X" && position[1] == "X" && position[2] == "X"
+  WIN_COMBINATIONS.each do |win_combination|
+    index_0 = win_combination[0]
+    index_1 = win_combination[1]
+    index_2 = win_combination[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+    
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
         return win_combination
-      else
-        false
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination
       end
-    end
+      return false
   end
-end
 
 def full?(board)
   board.all? do |position|
