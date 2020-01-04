f006230460eff85004351b2a1aaa1386dd3a7c4b
Mark Drehmann <markdrehmann@gmail.com>
2020-01-04 02:19:03 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -40,11 +40,11 @@ def full?(board)
 end
 
 def draw?(board)
-  if full? == true && won? == false
+  if full?(board) == true && won?(board) == false
     true
-  elsif full? == false && won? == false
+  elsif full?(board) == false && won?(board) == false
     false
-  elsif won? == true
+  elsif won?(board) == true
     false
   end
 end
