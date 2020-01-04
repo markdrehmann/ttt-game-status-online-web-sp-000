7dcc5101ebe9b416820144ce703a347d03e3bc12
Mark Drehmann <markdrehmann@gmail.com>
2020-01-04 01:14:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,3 +13,15 @@ WIN_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6]
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_index|
+    win_index.each do |position|
+      if position[0] == "X" && position[1] == "X" && position[2] == "X"
+        return WIN_COMBINATIONS
+      else
+        false
+      end
+    end
+  end
+end
